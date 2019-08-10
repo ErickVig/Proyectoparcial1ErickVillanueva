629101fef2924aa2941f1df46d5871075420563c
ErickVig <gudielerick11@gmail.com>
2019-08-09 22:45:13 -0600

    subiendo

---
+++ datos.php
@@ -0,0 +1,14 @@
+<?php
+    echo $nombre=$_POST['nombre'], "<br>";
+    echo $apellido=$_POST['apellido'], "<br>";
+    echo $cui=$_POST['cui'], "<br>";
+    echo $estadocivil=$_POST['estadocivil'], "<br>";
+    echo $fechan=$_POST['fechan'], "<br>";
+    echo $telefono=$_POST['telefono'], "<br>";
+    echo $nit=$_POST['nit'], "<br>";
+    echo $genero=$_POST['genero'], "<br>";
+    echo $correo=$_POST['correo'], "<br>";
+    echo $cargo=$_POST['cargo'], "<br>";
+    echo $grado=$_POST['grado'], "<br>";
+    echo $experiencia=$_POST['experiencia'], "<br>";
+?>
